02509b33956d150807720c4937dc78d67d0461ed
Jacob Foshee <jacobf@gmail.com>
2011-01-15 20:58:21 -0600

    added basic animation

--- quad.js
+++ quad.js
@@ -45,13 +45,26 @@ function initBuffers() {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 }
 
+var elapsedTime = 0.0;
+var lastTime = 0.0;
+var squareRotation = 0.0;
+
+function updateGameState() {
+    squareRotation += (30 * elapsedTime) / 1000.0;
+}
+
 function drawScene() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     perspectiveMatrix = makePerspective(45, 1.0, 0.1, 100.0);
 
     loadIdentity();
-    mvTranslate([-0.0, 0.0, -3.0]);
+    mvPushMatrix();
+    mvTranslate([0.0, 0.0, -5.0]);  // To Center of quad
+    mvRotate(squareRotation, [0, 1, 0]);
+    mvTranslate([0.0, 0.0, +5.0]);  // From Center of quad
+
+    mvTranslate([0.0, 0.0, -3.0]);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
     gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
@@ -60,7 +73,12 @@ function drawScene() {
 
     setMatrixUniforms();
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+    mvPopMatrix();
 }
+
+var mvMatrix;
+var mvMatrixStack = [];
 
 function loadIdentity() {
     mvMatrix = Matrix.I(4);
@@ -74,12 +92,51 @@ function mvTranslate(v) {
     multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
 }
 
+function mvPushMatrix(m) {
+    if (m) {
+        mvMatrixStack.push(m.dup());
+        mvMatrix = m.dup();
+    } else {
+        mvMatrixStack.push(mvMatrix.dup());
+    }
+}
+
+function mvPopMatrix() {
+    if (!mvMatrixStack.length) {
+        throw ("Can't pop from an empty matrix stack.");
+    }
+
+    mvMatrix = mvMatrixStack.pop();
+    return mvMatrix;
+}
+
+function mvRotate(angle, v) {
+    var inRadians = angle * Math.PI / 180.0;
+
+    var m = Matrix.Rotation(inRadians, $V([v[0], v[1], v[2]])).ensure4x4();
+    multMatrix(m);
+}
+
 function setMatrixUniforms() {
     var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
 
     var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
     gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
+
+function updateTime() {
+    var currentTime = (new Date).getTime();
+    if (lastTime) {
+        elapsedTime = currentTime - lastTime;
+    }
+    lastTime = currentTime;
+}
+
+function updateGame() {
+    updateTime();
+    updateGameState();
+    drawScene();
 }
 
 function main() {
@@ -97,5 +154,5 @@ function main() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    drawScene();
+    setInterval("updateGame()", 1000 / 60.0);
 }
