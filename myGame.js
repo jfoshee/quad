ca20a6a70f9e1f429b91f4979cc41e128e62cd3b
Jacob Foshee <jacobf@gmail.com>
2011-01-16 01:49:37 -0600

    Added texture mapping.  Firefox 4 wins in terms of helping track down WebGL errors.

--- myGame.js
+++ myGame.js
@@ -19,8 +19,11 @@ function loadShaders() {
     vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(vertexPositionAttribute);
 
-    vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-    gl.enableVertexAttribArray(vertexColorAttribute);
+//    vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+//    gl.enableVertexAttribArray(vertexColorAttribute);
+
+    textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+    gl.enableVertexAttribArray(textureCoordAttribute);
 }
 
 function loadModel() {
@@ -97,8 +100,65 @@ function loadVertexIndices() {
     return cubeVertexIndices;
 }
 
+function loadTextureCoordinates() {
+    var textureCoordinates = [
+    // Front
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Back
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Top
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Bottom
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Right
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+    // Left
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0
+  ];
+    return textureCoordinates;
+}
+
+function handleLoadedTexture(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+var myTexture;
+function loadTexture() {
+    myTexture = gl.createTexture();
+    myTexture.image = new Image();
+    myTexture.image.onload = function () {
+        handleLoadedTexture(myTexture)
+    }
+    myTexture.image.src = "texture.png";
+}
+
 function myLoadAssets() {
     loadShaders();
+    loadTexture();
 
     var vertices = loadModel();
     vertexBuffer = gl.createBuffer();
@@ -114,6 +174,11 @@ function myLoadAssets() {
     indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexIndices), gl.STATIC_DRAW);
+
+    var textureCoords = loadTextureCoordinates();
+    textureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
 }
 
 var squareRotation = 0.0;
@@ -134,24 +199,39 @@ function myDrawScene(gl) {
     mvPushMatrix();
     mvTranslate([0.0, 0.0, -5.0]);
     mvRotate(squareRotation, [0.5, 1, 0]);
-    setMatrixUniforms();
+    setShaderUniformParameters();
 
+    // Positions
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+
+    // Texture Coords
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, myTexture);
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+    // Colors
+    //gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    //gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+
+    // Vertex Indices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    // Draw
     gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
 
     mvPopMatrix();
 }
 
-function setMatrixUniforms() {
+function setShaderUniformParameters() {
     var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
 
     var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
     gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
 }
 
 function startMyGame() {
